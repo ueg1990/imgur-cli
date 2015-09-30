77cb45ef86359cd9ad41e2bd99b80d8c82e29084
ueg1990 <uehtesham90@gmail.com>
2015-09-30 00:02:14 -0400

    Add unit test for imgur credentials using config file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,8 @@ import fixtures
 import imgurpython
 import testtools
 
+from unittest import mock
+
 import imgur_cli.cli as cli
 
 FAKE_ENV = {'IMGUR_CLIENT_ID': 'client_id',
@@ -15,26 +17,31 @@ FAKE_ENV = {'IMGUR_CLIENT_ID': 'client_id',
 
 class TestImgurCli(testtools.TestCase):
 
-    def make_env(self, exclude=None):
-        if not exclude:
-            exclude = []
-        env = {key: value for key, value in FAKE_ENV.items() if key not in exclude}
-        self.useFixture(fixtures.MonkeyPatch('os.environ', env))
+    def make_env(self, attribute, exclude=None):
+        env = {key: value for key, value in FAKE_ENV.items() if key != exclude}
+        self.useFixture(fixtures.MonkeyPatch(attribute, env))
 
-    def test_imgur_credentials_env(self):
-        self.make_env()
+    def _test_imgur_credentials(self, attribute):
+        self.make_env(attribute)
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     'mashape_key')
         imgur_credentials = cli.imgur_credentials()
         self.assertEqual(expected, imgur_credentials)
-        self.make_env(exclude=['IMGUR_MASHAPE_KEY'])
+        self.make_env(attribute, exclude='IMGUR_MASHAPE_KEY')
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     None)
         imgur_credentials = cli.imgur_credentials()
         self.assertEqual(expected, imgur_credentials)
-        self.make_env(exclude=['IMGUR_CLIENT_ID'])
+        self.make_env(attribute, exclude='IMGUR_CLIENT_ID')
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
-        self.make_env(exclude=['IMGUR_CLIENT_SECRET'])
+        self.make_env(attribute, exclude='IMGUR_CLIENT_SECRET')
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
+
+    def test_imgur_credentials_env(self):
+        self.useFixture(fixtures.MonkeyPatch('imgur_cli.cli.config', None))
+        self._test_imgur_credentials('os.environ')
+
+    def test_imgur_credentials_config(self):
+        self._test_imgur_credentials('imgur_cli.cli.config')
