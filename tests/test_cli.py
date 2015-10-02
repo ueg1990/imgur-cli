398ec3ecb95132245b6768ad7ee6c09d2e901431
ueg1990 <uehtesham90@gmail.com>
2015-10-02 00:33:31 -0400

    Remove unit test for config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,31 +17,25 @@ FAKE_ENV = {'IMGUR_CLIENT_ID': 'client_id',
 
 class TestImgurCli(testtools.TestCase):
 
-    def make_env(self, attribute, exclude=None):
+    def make_env(self, exclude=None):
         env = {key: value for key, value in FAKE_ENV.items() if key != exclude}
-        self.useFixture(fixtures.MonkeyPatch(attribute, env))
+        self.useFixture(fixtures.MonkeyPatch('os.environ', env))
 
-    def _test_imgur_credentials(self, attribute):
-        self.make_env(attribute)
+    def test_imgur_credentials_env(self):
+        self.useFixture(fixtures.MonkeyPatch('imgur_cli.cli.config', None))
+        self.make_env()
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     'mashape_key')
         imgur_credentials = cli.imgur_credentials()
         self.assertEqual(expected, imgur_credentials)
-        self.make_env(attribute, exclude='IMGUR_MASHAPE_KEY')
+        self.make_env(exclude='IMGUR_MASHAPE_KEY')
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     None)
         imgur_credentials = cli.imgur_credentials()
         self.assertEqual(expected, imgur_credentials)
-        self.make_env(attribute, exclude='IMGUR_CLIENT_ID')
+        self.make_env(exclude='IMGUR_CLIENT_ID')
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
-        self.make_env(attribute, exclude='IMGUR_CLIENT_SECRET')
+        self.make_env(exclude='IMGUR_CLIENT_SECRET')
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
-
-    def test_imgur_credentials_env(self):
-        self.useFixture(fixtures.MonkeyPatch('imgur_cli.cli.config', None))
-        self._test_imgur_credentials('os.environ')
-
-    def test_imgur_credentials_config(self):
-        self._test_imgur_credentials('imgur_cli.cli.config')
