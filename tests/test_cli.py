3ee0e42d0bef2f8f11a0154d420478b0d3b5e0ab
ueg1990 <uehtesham90@gmail.com>
2015-10-14 01:08:17 -0400

    Add test for endpoint /album

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 import fixtures
@@ -16,21 +17,24 @@ FAKE_ENV = {'IMGUR_CLIENT_ID': 'client_id',
             'IMGUR_MASHAPE_KEY': 'mashape_key'}
 
 
+@mock.patch('imgur_cli.cli.imgurpython.ImgurClient')
+@mock.patch('imgur_cli.cli_api.generate_output')
 class TestImgurCli(testtools.TestCase):
 
     def setUp(self):
         super(TestImgurCli, self).setUp()
 
-    def cli(self, argv):
+    def cli(self, argv, exclude=None):
+        self.make_env(exclude)
         _cli = cli.ImgurCli()
-        return _cli.main(argv.split())
+        _cli.main(argv)
+        return _cli
 
     def make_env(self, exclude=None):
         env = {key: value for key, value in FAKE_ENV.items() if key != exclude}
         self.useFixture(fixtures.MonkeyPatch('os.environ', env))
 
-    def test_imgur_credentials_env(self):
-        self.useFixture(fixtures.MonkeyPatch('imgur_cli.cli.config', None))
+    def test_imgur_credentials_env(self, mock_client, mock_output):
         self.make_env()
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     'mashape_key')
@@ -48,5 +52,15 @@ class TestImgurCli(testtools.TestCase):
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
 
-    def test_help_unknown_command(self):
-        self.assertRaises(exceptions.CommandError, self.cli, 'help foofoo')
+    def test_help_unknown_command(self, mock_client, mock_output):
+        self.assertRaises(exceptions.CommandError, self.cli, ['help', 'foofoo'])
+
+    def test_album(self, mock_client, mock_output):
+        argv = ['album', '123']
+        _cli = self.cli(argv)
+        parser_args = _cli.parser.parse_args(argv)
+        self.assertTrue(argv[0] in _cli.subcommands)
+        self.assertEqual(parser_args.album_id, argv[1])
+        self.assertEqual(parser_args.func.__name__, 'cmd_album')
+        self.assertTrue(_cli.client.get_album.called)
+        self.assertRaises(SystemExit, self.cli, ['album'])
