4fac405bbbdd508a172e3d86963fa48d881e0cad
ueg1990 <uehtesham90@gmail.com>
2015-10-14 01:21:41 -0400

    Move class level mock patch to setUp and tearDown

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,12 +17,19 @@ FAKE_ENV = {'IMGUR_CLIENT_ID': 'client_id',
             'IMGUR_MASHAPE_KEY': 'mashape_key'}
 
 
-@mock.patch('imgur_cli.cli.imgurpython.ImgurClient')
-@mock.patch('imgur_cli.cli_api.generate_output')
 class TestImgurCli(testtools.TestCase):
 
     def setUp(self):
         super(TestImgurCli, self).setUp()
+        self.mock_client = mock.patch('imgur_cli.cli.imgurpython.ImgurClient')
+        self.mock_client.start()
+        self.mock_output = mock.patch('imgur_cli.cli_api.generate_output')
+        self.mock_output.start()
+
+    def tearDown(self):
+        super(TestImgurCli, self).tearDown()
+        self.mock_client.stop()
+        self.mock_output.stop()
 
     def cli(self, argv, exclude=None):
         self.make_env(exclude)
@@ -34,7 +41,7 @@ class TestImgurCli(testtools.TestCase):
         env = {key: value for key, value in FAKE_ENV.items() if key != exclude}
         self.useFixture(fixtures.MonkeyPatch('os.environ', env))
 
-    def test_imgur_credentials_env(self, mock_client, mock_output):
+    def test_imgur_credentials_env(self):
         self.make_env()
         expected = ('client_id', 'client_secret', 'access_token', 'refresh_token',
                     'mashape_key')
@@ -52,10 +59,10 @@ class TestImgurCli(testtools.TestCase):
         self.assertRaises(imgurpython.client.ImgurClientError,
                           cli.imgur_credentials)
 
-    def test_help_unknown_command(self, mock_client, mock_output):
+    def test_help_unknown_command(self):
         self.assertRaises(exceptions.CommandError, self.cli, ['help', 'foofoo'])
 
-    def test_album(self, mock_client, mock_output):
+    def test_album(self):
         argv = ['album', '123']
         _cli = self.cli(argv)
         parser_args = _cli.parser.parse_args(argv)
@@ -65,7 +72,7 @@ class TestImgurCli(testtools.TestCase):
         self.assertTrue(_cli.client.get_album.called)
         self.assertRaises(SystemExit, self.cli, ['album'])
 
-    def test_image(self, mock_client, mock_output):
+    def test_image(self):
         argv = ['image', '123']
         _cli = self.cli(argv)
         parser_args = _cli.parser.parse_args(argv)
