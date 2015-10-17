c4ee061f62e34c70cc67286ed0291423353cbcbe
ueg1990 <uehtesham90@gmail.com>
2015-10-17 04:03:31 -0400

    Define function and decorators for subparsers

--- imgur_cli/utils.py
+++ imgur_cli/utils.py
@@ -10,7 +10,7 @@ def cli_arg(*args, **kwargs):
 
 
 def add_arg(func, *args, **kwargs):
-    """Bind CLI arguments a 'cmd_' format function"""
+    """Bind CLI arguments to a 'cmd_' format function"""
     if not hasattr(func, 'arguments'):
         func.arguments = []
 
@@ -18,6 +18,20 @@ def add_arg(func, *args, **kwargs):
         # Because of the semantics of decorator composition if we just append
         # to the options list positional options will appear to be backwards.
         func.arguments.insert(0, (args, kwargs))
+
+
+def cli_subparser(*args, **kwargs):
+    """Decorator for CLI subparsers"""
+    def _decorator(func):
+        add_subparser(func, *args, **kwargs)
+        return func
+    return _decorator
+
+
+def add_subparser(func, *args, **kwargs):
+    """Bind CLI subparsers to a 'subparser_' format function"""
+    if not hasattr(func, 'subparser'):
+        func.subparser = args[0]
 
 
 def generate_output(result, output_filename=None):
