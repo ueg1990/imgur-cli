09cabdc90d1404475a0afb511e9c4e6eccd6eca3
ueg1990 <uehtesham90@gmail.com>
2015-10-18 15:03:13 -0400

    Define logic for album create

--- imgur_cli/cli_api.py
+++ imgur_cli/cli_api.py
@@ -2,9 +2,9 @@ import imgurpython
 
 from imgur_cli import exceptions
 from imgur_cli.utils import cli_arg
-from imgur_cli.utils import generate_output
-
 from imgur_cli.utils import cli_subparser
+from imgur_cli.utils import data_fields
+from imgur_cli.utils import generate_output
 
 
 SUBPARSERS = {'gallery': 'Gallery subparser', 'album': 'Album subparser',
@@ -129,10 +129,42 @@ def cmd_album_id(client, args):
 @cli_arg('--output-file', default=None, metavar='<output_file>',
          help='Save output to a JSON file')
 def cmd_images(client, args):
-    """Get information about a specific album"""
+    """Return all of the images in the album"""
     album_images = client.get_album_images(args.album_id)
     data = [item.__dict__ for item in album_images]
     generate_output({'album_images': data}, args.output_file)
+
+
+@cli_subparser('album')
+@cli_arg('--ids', metavar='<ids>', help='Comma separated list of image ids that you '
+         'want to be included in the album; you have to be logged in as the user '
+         'for adding the image ids')
+@cli_arg('--title', metavar='<title>', help='The title of the album')
+@cli_arg('--description', metavar='<description>',
+         help='The description of the album')
+@cli_arg('--privacy', metavar='<privacy>', choices=['public', 'hidden', 'secret'],
+         help="Sets the privacy level of the album."
+              "Values are : public | hidden | secret."
+              "Defaults to user's privacy settings for logged in users")
+@cli_arg('--layout', metavar='<layout>',
+         choices=['blog', 'grid', 'horizontal', 'vertical'],
+         help='Sets the layout to display the album. '
+         'Values are : blog | grid | horizontal | vertical')
+@cli_arg('--cover', metavar='<cover>',
+         help='The ID of an image that you want to be the cover of the album; '
+         'you have to be logged in as the user')
+def cmd_create(client, args):
+    """
+    Create a new album. Optional parameter of ids is an array of image ids to
+    add to the album; you have to be logged in as the user for adding the image ids.
+
+    This method is available without authenticating an account, and may be used
+    merely by sending "Authorization: Client-ID {client_id}" in the request headers.
+    Doing so will create an anonymous album which is not tied to an account
+    """
+    fields = data_fields(args, client.allowed_album_fields)
+    album = client.create_album(fields)
+    generate_output({'album': album})
 
 
 @cli_subparser('image')
@@ -146,7 +178,7 @@ def cmd_image_id(client, args):
 
 @cli_subparser('image')
 @cli_arg('type', choices=['file', 'url'],
-         help='The type of the file that\'s being sent; file, base64 or URL')
+         help="The type of the file that's being sent; file, base64 or URL")
 @cli_arg('image', help='A binary file, base64 data, or a URL for an image '
                        '(up to 10MB)')
 @cli_arg('--name', metavar='<name>', help='The name of the file, '
@@ -159,16 +191,12 @@ def cmd_image_id(client, args):
          help='The description of the image')
 def cmd_upload(client, args):
     """Upload a new image"""
-    config = {}
-    for field in client.allowed_image_fields:
-        field_value = getattr(args, field, None)
-        if field_value:
-            config[field] = field_value
+    config = data_fields(args, client.allowed_image_fields)
     if args.type == 'file':
-        output = client.upload_from_path(args.image, config)
+        image = client.upload_from_path(args.image, config)
     else:
-        output = client.upload_from_url(args.image, config)
-    generate_output({'image': output})
+        image = client.upload_from_url(args.image, config)
+    generate_output({'image': image})
 
 
 @cli_subparser('image')
