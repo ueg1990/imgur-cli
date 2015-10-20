02b2e30e5221ec67f3d41b82f85cdb75d59843c0
ueg1990 <uehtesham90@gmail.com>
2015-10-20 00:06:30 -0400

    Add unit test for album favorite

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -219,6 +219,13 @@ class TestImgurCli(testtools.TestCase):
         self.assertParser(_cli, parser_args, argv)
         self.assertTrue(_cli.client.album_delete.called)
 
+    def test_album_favorite(self):
+        argv = ['album', 'favorite', '123']
+        _cli = self.cli(argv)
+        parser_args = _cli.parser.parse_args(argv)
+        self.assertParser(_cli, parser_args, argv)
+        self.assertTrue(_cli.client.album_favorite.called)
+
     def test_album_set_images(self):
         argv = ['album', 'set-images', '123', 'abc']
         _cli = self.cli(argv)
